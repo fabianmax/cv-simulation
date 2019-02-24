4d35e5d34ebcd782ee727960f4a07b4ff89769ca
fabianmax <fabian.mueller@statworx.de>
2019-02-24 13:51:55 +0100

    Changed parameter-grid

--- run.R
+++ run.R
@@ -11,16 +11,16 @@ n_max <- 10000
 # XGBoost -----------------------------------------------------------------
 
 # Parameters
-params_xgb <- expand.grid(nrounds = c(50, 100, 150, 250, 500),
+params_xgb <- expand.grid(nrounds = c(250),
                           eta = c(0.3),
-                          gamma = 0,
+                          gamma = c(0, 2),
                           max_depth = c(3, 6, 9),
                           min_child_weight = 1,
-                          subsample = 1,
-                          colsample_bytree = c(0.8, 1),
+                          subsample = c(0.7, 1),
+                          colsample_bytree = c(0.7),
                           lambda = c(0, 1),
                           alpha = c(0, 1),
-                          early_stopping_rounds = 5,
+                          early_stopping_rounds = 3,
                           nthread = 6)
 
 # Run
@@ -32,14 +32,15 @@ result_xgb <- runs %>%
   map2(.x = ., .y = seq(length(.)), ~mutate(.x, id = .y)) %>% 
   bind_rows()
 
-path <- paste0("results/", Sys.Date(), "_simulation_results.RData")
-save(result, file = path)
+path_xgb <- paste0("results/", Sys.Date(), "_simulation_results_xgb.RData")
+save(result_xgb, file = path_xgb)
 
 
 # RandomForest ------------------------------------------------------------
 
 # Parameters
-params_rf <- expand.grid(trees = c(25, 50, 75, 100, 150, 200, 250))
+params_rf <- expand.grid(trees = c(25, 50, 75, 100, 150, 200, 250),
+                         mtry = c(3, 4, 6, 10))
 
 # Run
 result_rf <- runs %>% 
@@ -49,4 +50,7 @@ result_rf <- runs %>%
   map(~ run_experiment(n = .x, list(fit_rf, predict_rf), params_rf)) %>% 
   map2(.x = ., .y = seq(length(.)), ~mutate(.x, id = .y)) %>% 
   bind_rows()
+
+path_rf <- paste0("results/", Sys.Date(), "_simulation_results_rf.RData")
+save(result_rf, file = path_rf)
 
